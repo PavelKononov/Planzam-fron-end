0340e1f9029e9fb93ad4108ef4c7dc0c53cbfe47
oleg geraschenko <oleg@netformat.net>
2019-10-07 10:51:08 +0300

    update front elements

--- src/app/providers/auth/auth.ts
+++ src/app/providers/auth/auth.ts
@@ -2,7 +2,7 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 
-import {Storage} from '@ionic/Storage';
+// import {Storage} from '@ionic/Storage';
 
 
 @Injectable()
